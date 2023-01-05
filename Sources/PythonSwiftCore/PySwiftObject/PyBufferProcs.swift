31f79be11874bf4b0fd2e4754cb0626265832f24
psychowaspx <psychowaspx@gmail.com>
2023-01-05 18:57:39 +0100

    Update PyBufferProcs.swift

--- Sources/PythonSwiftCore/PySwiftObject/PyBufferProcs.swift
+++ Sources/PythonSwiftCore/PySwiftObject/PyBufferProcs.swift
@@ -20,13 +20,13 @@ public class PyBufferProcsHandler {
         
     }
     
-    init(getBuffer: PySwiftBuf_Get, releaseBuffer: PySwiftBuf_Release) {
+    init(_getBuffer: PySwiftBuf_Get, _releaseBuffer: PySwiftBuf_Release) {
         
         buffer_ptr = .allocate(capacity: 1)
         
         buffer_ptr.pointee = .init(
-            bf_getbuffer: unsafeBitCast(getBuffer, to: PyBuf_Get.self),
-            bf_releasebuffer: unsafeBitCast(releaseBuffer, to: PyBuf_Release.self)
+            bf_getbuffer: unsafeBitCast(_getBuffer, to: PyBuf_Get.self),
+            bf_releasebuffer: unsafeBitCast(_releaseBuffer, to: PyBuf_Release.self)
         )
         
     }
